03685aea8f130f5973b406741e6ca1958dfeb05c
Spencer Harston <spencer314@gmail.com>
2022-04-08 13:22:36 -0600

    feat: add images to results

--- script.py
+++ script.py
@@ -4,12 +4,14 @@ import re
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-page = r.get('https://repos.americafirst.com')
+afcu_url = 'https://repos.americafirst.com'
+
+page = r.get(afcu_url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 car_cards = []
-for element in soup.select('.card-body'):
-    car_title = element.select('.card-title')[0].text
+for element in soup.find_all('div', class_='card mb-3'):
+    car_title = element.select(selector='.card-title')[0].text
     car_details = []
     for li in element.select('ul.text-secondary li'):
         car_details.append(li.text)
@@ -28,12 +30,15 @@ for element in soup.select('.card-body'):
     
     car_bid_end_date = element.find(string=re.compile('Bidding Ends'))
 
+    car_image_src = "{}{}".format(afcu_url, element.select_one(selector='img').attrs['src'])
+
     car_info = {
         "title": car_title.strip(),
         "details": ' '.join(car_details),
         "bid_price": car_bid_price.strip(),
         "bin_price": car_bin_price.strip(),
-        "bid_end_date": car_bid_end_date[14:]
+        "bid_end_date": car_bid_end_date[14:],
+        "image": car_image_src
     }
 
     car_cards.append(car_info)
