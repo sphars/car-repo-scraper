105aeee38f8e403a1132d7d18d1c871d74d19600
Spencer Harston <spencer314@gmail.com>
2022-04-16 22:04:21 -0600

    feat: add car details URL

--- script.py
+++ script.py
@@ -14,35 +14,37 @@ def getAFCUCars():
     results = []
 
     for element in soup.find_all('div', class_='card mb-3'):
-        car_title = element.select(selector='.card-title')[0].text
+        car_title = element.select(selector='.card-title')[0].text.strip()
         car_details = []
         for li in element.select('ul.text-secondary li'):
             car_details.append(li.text)
 
         car_bid = element.find(string=re.compile('Current High Bid'))
         if car_bid:
-            car_bid_price = car_bid.parent.select('span')[0].text
+            car_bid_price = car_bid.parent.select('span')[0].text.strip()
         else:
             car_bid_price = ''
 
         car_bin = element.find(string=re.compile('Buy it Now'))
         if car_bin:
-            car_bin_price = car_bin.parent.select('span')[0].text
+            car_bin_price = car_bin.parent.select('span')[0].text.strip()
         else:
             car_bin_price = ''
 
-        car_bid_end_date = element.find(string=re.compile('Bidding Ends'))
+        car_bid_end_date = element.find(string=re.compile('Bidding Ends'))[14:]
 
-        car_image_src = "{}{}".format(afcu_url, element.select_one(selector='img').attrs['src'])
+        car_image_src = '{}{}'.format(afcu_url, element.select_one(selector='img').attrs['src'])
+
+        car_details_url = '{}{}'.format(afcu_url, element.select_one(selector='.card-footer a.btn.btn-primary').attrs['href'])
 
         car_info = {
-            'title': car_title.strip(),
+            'title': car_title,
             'details': ' '.join(car_details),
-            'bid_price': car_bid_price.strip(),
-            'bin_price': car_bin_price.strip(),
-            'bid_end_date': car_bid_end_date[14:],
+            'bid_price': car_bid_price,
+            'bin_price': car_bin_price,
+            'bid_end_date': car_bid_end_date,
             'image': car_image_src,
-            'url': '',
+            'url': car_details_url,
             'source': "AFCU"
         }
 
